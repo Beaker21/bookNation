154f6e253521dd2f83fa796e28824209ecc52e27
jacksonrwold <jacksonrwold@gmail.com>
2019-03-27 10:59:17 -0700

    commit

--- src/components/grid.js
+++ src/components/grid.js
@@ -5,7 +5,7 @@ export default class BookGrid extends Component {
         super(props)
 
         this.state = {
-            books: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+            books: []
         }
     }
 
