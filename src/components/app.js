1e5e6c206f35bbe9dedd0a28aea6076fc123f96d
galenmontague <gsmontaguefamily@yahoo.com>
2019-03-27 11:14:53 -0600

    Added account pages

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 
+import Account from "./account" 
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        <Account />
       </div>
     );
   }
