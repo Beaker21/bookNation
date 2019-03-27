001a707d044d73ef5c1e84e08bb1aea78d5e8e98
madisonjessop <madisonrjessop@gmail.com>
2019-03-27 11:15:10 -0600

    initial commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
+import CheckoutPage from './checkout';
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        <h1>Checkout</h1>
+
+        <CheckoutPage />
       </div>
     );
   }
