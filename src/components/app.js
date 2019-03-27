ff176a2272681c3d8756d445fbe9761d5b1302a9
croberts7 <ctheroberts@gmail.com>
2019-03-27 11:00:34 -0600

    Navbar and routing

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import Account from "./account";
+import Home from "./home";
+import NavBar from "./navbar";
+import Auth from "./authentication/auth";
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        <Router>
+          <div>
+          <NavBar />
+           <Switch>   
+              <Route path="/" component={Home} />
+              <Route path="/account" component={Account} />
+              <Route path="/auth" component={Auth} />
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
