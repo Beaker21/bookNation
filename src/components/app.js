9fbc6073226192098d1586dd65be65eec1367286
madisonjessop <madisonrjessop@gmail.com>
2019-03-27 13:42:45 -0600

    final on branch

--- src/components/app.js
+++ src/components/app.js
@@ -6,7 +6,6 @@ export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>Checkout</h1>
 
         <CheckoutPage />
       </div>
--- src/components/checkout.js
+++ src/components/checkout.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+import booksStack from "../../static/assets/images/books-stack.jpg";
 
 export default class CheckoutPage extends Component {
   constructor(props) {
@@ -11,7 +12,7 @@ export default class CheckoutPage extends Component {
       <div className="app-container">
         <div className="row">
           <div className="col">
-            <Item img="http://ecx.images-amazon.com/images/I/61%2BABMMN5zL._SL1500_.jpg" />
+            <Item img={booksStack} />
           </div>
           <div className="col no-gutters">
             <Checkout />
@@ -37,8 +38,15 @@ const Item = (props) => (
 const Checkout = (props) => (
  <div className="checkout">
     <div className="checkout-container">
-     <h3 className="heading-3">Credit card checkout</h3>
-     <Input label="Cardholder's Name" type="text" name="name" />
+     <h3 className="heading-3">Checkout</h3>
+     <Input label="Full Name" type="text" name="full_name" />
+     <Input label="Email" type="text" name="email" />
+     <Input label="Address Line 1" type="text" name="address_line_1" />
+     <Input label="Address Line 2" type="text" name="address_line_2" />
+     <Input label="City" type="text" name="city" />
+     <Input label="State" type="text" name="state" />
+     <Input label="Postal Code" type="text" name="postal_code" />
+     <br/>
      <Input label="Card Number" type="number" name="card_number" imgSrc="https://seeklogo.com/images/V/visa-logo-6F4057663D-seeklogo.com.png" />
       <div className="row">
         <div className="col">
